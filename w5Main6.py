b1e61d608e8a95ab3f1d24bb44f8e9d59e6a865e
HaeSeon <gildogi@naver.com>
2016-04-03 22:57:16 +0900

    Added files via upload

---
+++ w5Main6.py
@@ -0,0 +1,17 @@
+﻿a=raw_input('a input kawi, bawi, bo : ')
+b=raw_input('b input kawi, bawi, bo : ')
+if a=='kawi':
+    if b=='kawi':
+        print "%s" %"Draw"
+    elif b=='bo':
+        print "%s" %"kawi Win"
+    elif b=='bawi':
+        print "%s" %"bawi win"
+elif a=='bawi':
+    if b=='bo':
+        print "%s" %"bo Win"
+    elif b=='bawi':
+        print "%s" %"Draw"
+elif a=='bo':
+    if b=='bo':
+        print "%s" %"Draw"
