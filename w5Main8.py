44fdcfa34f6e1ca42db12d76dab19b9359e98685
HaeSeon <gildogi@naver.com>
2016-04-04 21:31:04 +0900

    BMI

---
+++ w5Main8.py
@@ -0,0 +1,12 @@
+﻿height = input("Height (m): ")
+weight = input("Weight (kg): ")
+bmi = weight/(height*height)
+print bmi
+if bmi < 18.5:
+    print("Underweight")
+elif 18.5 <= bmi < 25.0:
+    print("Normal Weight")
+elif 25.0 <= bmi < 30.0:
+    print("Overweight")
+else:
+    print("Obese")
