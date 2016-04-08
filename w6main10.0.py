e6cd69728a0f915ba9042b56cf42ad5245579b2c
HaeSeon <gildogi@naver.com>
2016-04-08 11:36:18 +0900

    Added files via upload

---
+++ w6main10.0.py
@@ -0,0 +1,7 @@
+﻿def leapYear():
+    year=raw_input("input year : ")
+    year=int(year)
+    if (year%4 == 0) and (year%100 !=0 or year%400==0):
+        print "leap year"
+    else :
+        print "normal year"
---
+++ w6main10.py
@@ -0,0 +1,26 @@
+﻿def upanddowngame():
+    trycount=0
+    
+    inputnumber=input("input number")
+    mynumber=input("my number")
+
+  
+    while inputnumber!=mynumber:
+        
+        if inputnumber>mynumber:
+                print "High!"
+                mynumber=input("Guess number!")
+                trycount=trycount+1
+        
+        if inputnumber<mynumber:
+                print "Low!"
+                mynumber=input("Guess number!")
+                trycount=trycount+1
+        
+        if inputnumber==mynumber:
+             print "Correct!"
+        
+   
+    
+        
+    print "trycount is "+str(trycount)
