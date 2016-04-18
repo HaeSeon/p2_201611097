7b78af91c8de516c9e8aafc08124f5a6c725439e
HaeSeon <gildogi@naver.com>
2016-04-18 15:53:31 +0900

    Added files via upload

---
+++ W7Main.py
@@ -0,0 +1,18 @@
+﻿def drawSquareAtSave(size, pos):
+    t1.penup()
+    t1.setpos(pos)
+    t1.pendown()
+    track=list()
+    for i in range(0,4):
+        track.appand(t1.pos())
+        t1.forward(size)
+        t1.right(90)
+    return tracks
+
+
+def lab7():
+    mytrack=drawSquareAtSave(100,(0,0))
+    print mytrack
+    
+def main():
+    lab()
