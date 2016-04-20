695c6ba86a9a363fad97c299e0959a71b147de61
HaeSeon <gildogi@naver.com>
2016-04-20 13:57:48 +0900

    완성본!

---
+++ w7main22.py
@@ -0,0 +1,58 @@
+﻿def saveTracks():
+    import turtle
+    wn=turtle.Screen()
+    t1=turtle.Turtle()
+    t1.speed(5)
+    t1.penup()
+
+    mytracks=list()
+
+    t1.goto(-400,300)
+    mytracks.append(t1.pos())
+
+    t1.pendown()
+    t1.pencolor("Red")
+    t1.right(90)
+    t1.fd(400)
+    mytracks.append(t1.pos())
+    t1.backward(150)
+    t1.left(90)
+    t1.fd(300)
+    mytracks.append(t1.pos())
+    t1.left(90)
+    t1.fd(300)
+    mytracks.append(t1.pos())
+    t1.back(150)
+    mytracks.append(t1.pos())
+    t1.right(90)
+    t1.fd(300)
+    mytracks.append(t1.pos())
+    t1.left(90)
+    t1.right(90)
+    t1.right(90)
+    t1.fd(200)
+    mytracks.append(t1.pos())
+    t1.fd(300)
+    mytracks.append(t1.pos())
+    t1.back(100)
+    mytracks.append(t1.pos())
+    t1.left(90)
+    t1.fd(200)
+    mytracks.append(t1.pos())
+    return mytracks
+
+def replayTracks(mytracks):
+
+    for t in range(0,len(mytracks)):
+        
+        print mytracks[t]
+
+def lab7():
+    mytracks=saveTracks()
+    replayTracks(mytracks)
+    
+def main():
+    lab7()
+ 
+if __name__=="__main__":
+    main()  
